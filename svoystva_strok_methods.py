1db7f1508bd4d0594e3d98d6437ef59a8adc9908
bootdrv <57584904+bootdrv@users.noreply.github.com>
2020-01-08 17:31:28 +0200

    Add files via upload

---
+++ svoystva_strok_methods.py
@@ -0,0 +1,9 @@
+# Concatanation + 1 simb another
+HP = 'Hello Python!'
+new_str1 = (HP[6] + 'a'+ HP[8] + HP[9])
+print(new_str1)
+# multiplication
+new_str2 = 'z'
+# method UPPER
+print(new_str2 * 7)
+print(new_str2.upper() * 7)
