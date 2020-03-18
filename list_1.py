1560854870ac3518cddb2351e444b862f4122d33
bootdrv <57584904+bootdrv@users.noreply.github.com>
2020-03-18 11:25:52 +0200

    Add files via upload

---
+++ list_1.py
@@ -0,0 +1,48 @@
+# number_list = [32, 21, 48, 45.67]
+# print(number_list)
+# some_list = [12, 35.466, "Hello!"]
+# print(some_list)
+# print(len(some_list))
+# print(some_list[1])
+# another_list = some_list[:2]
+# print(another_list)
+# some_list[2] = "Hi Drive!"
+# print(some_list)
+# new_list = some_list+another_list
+# print(new_list)
+# # adding items
+# # new_list[5] = 'new word'
+# new_list.append('new word')
+# new_list.insert(0, 'start')
+# new_list.insert(2, 13)
+# # Removing items
+# # new_list.pop(-1)
+# # new_list.pop(0)
+# # new_list.pop(-3)
+# # deleted_item = new_list.pop()
+# deleted_item = new_list.remove(12)
+# print(deleted_item)
+# number_list = [12, 45, 3, -456, 23]
+# letter_list = ['s', 'a', 'd', 'w', 's']
+# method sortirovka
+# x = number_list.sort()
+# # new_list = letter_list.sort()
+# letter_list.sort()
+# new_list = letter_list
+# # print(number_list)
+# # print(letter_list)
+# # print(x)
+# # print(new_list)
+# number_list.reverse()
+# letter_list.reverse()
+# number_list.append(letter_list)
+# print(number_list)
+# print(letter_list)
+
+# Zadaniye Yura
+main_list = [12, 32, 45.246, 'Hi!']
+main_list_II = [34, 46, 47.247, 'Hello!']
+# change_list = main_list_II[0:3]   более длинная выборка
+# так короче
+change_list = main_list_II[:3]
+print(change_list)
