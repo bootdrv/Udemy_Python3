90e85402cf0999b265830f596337195cbc3a4862
bootdrv <57584904+bootdrv@users.noreply.github.com>
2020-03-24 16:22:41 +0200

    Add files via upload

--- tuples.py
+++ tuples.py
@@ -20,12 +20,12 @@ person_tuple = ('John', 'Smith', 1974)
 x = y = z = 12
 addon = x, y, z = 12, 13, 14
 print(x, y, z)
-# sozdayem peremennuyu dlia izvlecheniya dannih i prisvaivayem  person_tuple
+# создаем переменную для извлечения данных и присваиваем person_tuple
 first_name, last_name, year_of_birth = person_tuple
-# vivodim na ekran vse znacheniya peremennih
+# выводим на экран все значения переменных
 print(first_name, last_name, year_of_birth)
 t1 = (1, 2, 3, 4, 5, 7, 9, 1 )
-# vizivayem v tuple metod count - on vozvraschayet kolichestvo elementov
+# вызываем в  tuple метод count - он возвращает количество элементов
 print(t1.count(5))
 print(t1.count(1))
 greetings_tuple = ('hello', 'hi', 'hey', 'hi')
@@ -34,9 +34,25 @@ print(greetings_tuple.count('hello'))
 print(greetings_tuple.count('hodo'))
 #vichisliayem index
 print(t1.index(5))
-print(greetings_tuple.index('hi')) # vichislit index povtoriayuschihsia znacheniy
-# -vivedet index pervogo znacheniya- blijayshego iz odinakovix
+print(greetings_tuple.index('hi')) # вычисляем индекс повторяющихся значений
+# -выведется индекс первого значения из всех повторяющихся- ближайшего из одинаковых
 
 
-
+# zadaniye ot yuriya
+# Создайте объект tuple, описывающий компьютер и распакуйте его в
+# соответствующие переменные. Выведите переменные вызвав функцию print() один раз
+pc_tuple = (
+    #komjplektuha
+    'case: logic-power',
+    'Mb: Asus',
+    'cpu: intel Core i7',
+    'ram: kingston ddr 4 8Gb',
+    'video: Geforce 9900',
+    'hdd: tochiba 1TB',
+    'dvd: lg dvd-ram black',
+    'monitor: samsung 23',
+    'keyb: logitech 104 k',
+    'mouse: a4tech 3b')
+case, mb, cpu, ram, video_card, hdd, dvd_rw, monitor, keyb, mouse = pc_tuple
+print('\n'.join (pc_tuple)) #выводим каждое значение с новой строки в столбик
 
