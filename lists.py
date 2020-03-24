29d9d4b020d466dac3131564fbe852cbe8854cc3
bootdrv <57584904+bootdrv@users.noreply.github.com>
2020-03-24 14:42:58 +0200

    Add files via upload

---
+++ lists.py
@@ -0,0 +1,13 @@
+# print list
+number_list = [2, 4, 6, 10, 10.23, 'num']
+print(number_list)
+# print list2
+some_list = [10, 12, 'hello']
+print(some_list)
+# lenght
+print(len(some_list))
+# indexing
+print(number_list[2])
+#slicing
+print(some_list[-2])
+
