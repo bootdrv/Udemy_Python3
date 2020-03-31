66ae41d2f98cbbb6d3160d0bfa904e9cd2961ecd
bootdrv <57584904+bootdrv@users.noreply.github.com>
2020-03-31 15:44:51 +0300

    Add files via upload

---
+++ logical_operators.py
@@ -0,0 +1,28 @@
+# x = 1
+# y = 2
+
+# print(x>1)
+# print(y>1)
+
+# and , or , not
+# print(x>1 and y>1) # 'и'
+# print(x>1 or y>1) #  'или'
+# print(not x>1)
+# print(not y>1)
+
+print(True and True)
+print(True or True)
+print(not True)
+
+print(False and False)
+print(False or False)
+print(not False)
+
+print(True and False)
+print(True or False)
+
+name = 'John'
+age = 21
+is_married = False
+if age > 18 and is_married == False:
+    print('Hi {}! You can find a girl of your dream here'.format(name))
