d3bcbfde62ac84b5023613d04066f899af6be9af
bootdrv <57584904+bootdrv@users.noreply.github.com>
2020-03-31 15:22:19 +0300

    Add files via upload

---
+++ boolean.py
@@ -0,0 +1,70 @@
+# print(1<2)
+# print(type(True))
+# print(type(False))
+
+# Comparison operations
+
+#
+# print(1 == 1)
+# print( 1 == 2)
+# print( 'Hello' == "Hello")
+# print( 'Hello' == "hello")
+# print(1 != 1)    # знак не равенства -  !=
+# print( 'Hello' != "hello")
+# print(1>2)
+# print(1<2)
+# print(2>=2)
+# print(3>=2)
+# print(2<=2)
+# print(3>=2)
+# print(3<=2)
+# # сравнение строк и кодов символов (см. ascII)
+# print(ord('a'))
+# print(ord('b'))
+# print('a' > 'b')
+# print('hi' > 'Hello')
+# print(ord('i'))
+# print(ord('e'))
+
+# сравнение переменных
+# x = 10
+# y = 25
+# z = 10
+# print(x > y)
+# print(x < y)
+# print(x==y)
+# print(x!=y)
+# print(x==z)
+# age = int(input('input your age'))
+# print('Access is permitted: ' + str(age >= 18))
+
+
+# Задание от Юрия
+# part one
+a = 25
+b = 26
+print(a>b)
+print(a<b)
+print(a==b)
+print(a!=b)
+print(a>=b)
+print(a<=b)
+#part two
+
+simb = 'Z'
+simb_1 = 'z'
+print(ord(simb))
+print(ord(simb_1))
+print('z'<'Z')
+print('z'<='Z')
+print('z'>'Z')
+print('z'>='Z')
+print('z'!='Z')
+print('z'=='Z')
+
+
+
+
+
+
+
