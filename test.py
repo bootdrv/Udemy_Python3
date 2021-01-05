24e562f341d4660d953cb77d70634298c29cb2ec
bootdrv <57584904+bootdrv@users.noreply.github.com>
2021-01-05 13:55:58 +0200

    Add files via upload

---
+++ test.py
@@ -0,0 +1,50 @@
+print(2 + 2)
+print(2 - 2)
+print(2 * 2)
+print(2 / 2)
+print(2 // 2)
+print(3 // 2)
+print(2 ** 3)
+print(5 % 3)
+
+# next
+
+x = 5
+print(x)
+x = 'hello'
+print(x)
+
+# next
+greeting = 'hello'
+print(greeting)
+first_name = "Jack"
+last_name = 'White'
+print(greeting + first_name + last_name)
+long_string = 'this is Loooong string'
+print(long_string)
+# Escaping
+some_string = 'I\'m a programmer'
+print(some_string)
+another_string = "I want to learn \"Python\""
+print(another_string)
+another_string = 'I want to learn "Python"'
+print(another_string)
+string_w_new_line = "hello \nMy name is Kostya"
+print(string_w_new_line)
+numbers = '1\t23\t4567'
+print(numbers)
+some_text = "\thello! \n\tI'm very glad too see you!"
+print(some_text)
+
+# tripple quots
+
+string_tipple = """This is ' text with "triple quotes" """
+print(string_tipple)
+string_second = '''This is ' text with "triple quotes" '''
+print(string_second)
+
+# Indexing & Slicing
+greeting = 'Hello Python!'
+greeting_lenght = len(greeting)
+print(greeting_lenght)
+print(greeting[0])
